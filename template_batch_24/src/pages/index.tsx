ba9a6267c5728b9bd53c587e13d6b2596dc841a1
naufalfirdaus <naufalfirdausa@gmail.com>
2023-07-26 11:29:19 +0700

    master FE

---
+++ template_batch_24/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import LandingPage from "./component/layout/LandingPage";
+
+export default function Home() {
+  return (
+    <div>
+      <LandingPage>
+      <>Home</>
+      </LandingPage>
+    </div>
+  )
+}
