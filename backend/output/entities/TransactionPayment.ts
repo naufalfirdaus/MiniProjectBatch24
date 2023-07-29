77c515554b5e314d30a30d03ce51628c9bb0fa3c
mattgan31 <alamsyahr00@gmail.com>
2023-07-29 13:18:13 +0700

    Update transaction payment & users entities for relation

--- backend/output/entities/TransactionPayment.ts
+++ backend/output/entities/TransactionPayment.ts
@@ -1,61 +1,74 @@
-import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Users } from "./Users";
 
-@Index('transaction_payment_trpa_code_number_key', ['trpaCodeNumber'], {
+@Index("transaction_payment_trpa_code_number_key", ["trpaCodeNumber"], {
   unique: true,
 })
-@Index('transaction_payment_pkey', ['trpaId'], { unique: true })
-@Entity('transaction_payment', { schema: 'payment' })
+@Index("transaction_payment_pkey", ["trpaId"], { unique: true })
+@Entity("transaction_payment", { schema: "payment" })
 export class TransactionPayment {
-  @PrimaryGeneratedColumn({ type: 'integer', name: 'trpa_id' })
+  @PrimaryGeneratedColumn({ type: "integer", name: "trpa_id" })
   trpaId: number;
 
-  @Column('character varying', {
-    name: 'trpa_code_number',
+  @Column("character varying", {
+    name: "trpa_code_number",
     nullable: true,
     unique: true,
     length: 55,
+    default: () => "pay_id()",
   })
   trpaCodeNumber: string | null;
 
-  @Column('character varying', {
-    name: 'trpa_order_number',
+  @Column("character varying", {
+    name: "trpa_order_number",
     nullable: true,
     length: 25,
   })
   trpaOrderNumber: string | null;
 
-  @Column('numeric', { name: 'trpa_debit', nullable: true })
+  @Column("numeric", { name: "trpa_debit", nullable: true })
   trpaDebit: string | null;
 
-  @Column('numeric', { name: 'trpa_credit', nullable: true })
+  @Column("numeric", { name: "trpa_credit", nullable: true })
   trpaCredit: string | null;
 
-  @Column('character varying', {
-    name: 'trpa_type',
+  @Column("character varying", {
+    name: "trpa_type",
     nullable: true,
     length: 15,
   })
   trpaType: string | null;
 
-  @Column('character varying', {
-    name: 'trpa_note',
+  @Column("character varying", {
+    name: "trpa_note",
     nullable: true,
     length: 255,
   })
   trpaNote: string | null;
 
-  @Column('timestamp without time zone', {
-    name: 'trpa_modified_date',
+  @Column("timestamp without time zone", {
+    name: "trpa_modified_date",
     nullable: true,
+    default: () => "now()",
   })
   trpaModifiedDate: Date | null;
 
-  @Column('character varying', { name: 'trpa_source_id', length: 25 })
+  @Column("character varying", { name: "trpa_source_id", length: 25 })
   trpaSourceId: string;
 
-  @Column('character varying', { name: 'trpa_target_id', length: 25 })
+  @Column("character varying", { name: "trpa_target_id", length: 25 })
   trpaTargetId: string;
 
-  @Column('integer', { name: 'trpa_user_entity_id', nullable: true })
-  trpaUserEntityId: number | null;
+  @ManyToOne(() => Users, (users) => users.transactionPayments)
+  @JoinColumn([
+    { name: "trpa_user_entity_id", referencedColumnName: "userEntityId" },
+  ])
+  users: Users;
 }
--- backend/output/entities/Users.ts
+++ backend/output/entities/Users.ts
@@ -5,92 +5,150 @@ import {
   JoinColumn,
   OneToMany,
   OneToOne,
-} from 'typeorm';
-import { BusinessEntity } from './BusinessEntity';
-import { UsersAddress } from './UsersAddress';
-import { UsersEducation } from './UsersEducation';
-import { UsersEmail } from './UsersEmail';
-import { UsersExperiences } from './UsersExperiences';
-import { UsersLicense } from './UsersLicense';
-import { UsersMedia } from './UsersMedia';
-import { UsersPhones } from './UsersPhones';
-import { UsersRoles } from './UsersRoles';
-import { UsersSkill } from './UsersSkill';
+} from "typeorm";
+import { BatchTrainee } from "./BatchTrainee";
+import { BatchTraineeEvaluation } from "./BatchTraineeEvaluation";
+import { CartItems } from "./CartItems";
+import { Employee } from "./Employee";
+import { ProgramApply } from "./ProgramApply";
+import { ProgramApplyProgress } from "./ProgramApplyProgress";
+import { ProgramReviews } from "./ProgramReviews";
+import { SalesOrderHeader } from "./SalesOrderHeader";
+import { TransactionPayment } from "./TransactionPayment";
+import { BusinessEntity } from "./BusinessEntity";
+import { UsersAccount } from "./UsersAccount";
+import { UsersAddress } from "./UsersAddress";
+import { UsersEducation } from "./UsersEducation";
+import { UsersEmail } from "./UsersEmail";
+import { UsersExperiences } from "./UsersExperiences";
+import { UsersLicense } from "./UsersLicense";
+import { UsersMedia } from "./UsersMedia";
+import { UsersPhones } from "./UsersPhones";
+import { UsersRoles } from "./UsersRoles";
+import { UsersSkill } from "./UsersSkill";
 
-@Index('users_pkey', ['userEntityId'], { unique: true })
-@Index('users_user_name_key', ['userName'], { unique: true })
-@Entity('users', { schema: 'users' })
+@Index("users_pkey", ["userEntityId"], { unique: true })
+@Index("users_user_name_key", ["userName"], { unique: true })
+@Entity("users", { schema: "users" })
 export class Users {
-  @Column('integer', { primary: true, name: 'user_entity_id' })
+  @Column("integer", { primary: true, name: "user_entity_id" })
   userEntityId: number;
 
-  @Column('character varying', {
-    name: 'user_name',
+  @Column("character varying", {
+    name: "user_name",
     nullable: true,
     unique: true,
     length: 15,
   })
   userName: string | null;
 
-  @Column('character varying', {
-    name: 'user_password',
+  @Column("character varying", {
+    name: "user_password",
     nullable: true,
     length: 256,
   })
   userPassword: string | null;
 
-  @Column('character varying', {
-    name: 'user_first_name',
+  @Column("character varying", {
+    name: "user_first_name",
     nullable: true,
     length: 50,
   })
   userFirstName: string | null;
 
-  @Column('character varying', {
-    name: 'user_last_name',
+  @Column("character varying", {
+    name: "user_last_name",
     nullable: true,
     length: 50,
   })
   userLastName: string | null;
 
-  @Column('timestamp without time zone', {
-    name: 'user_birth_date',
+  @Column("timestamp without time zone", {
+    name: "user_birth_date",
     nullable: true,
   })
   userBirthDate: Date | null;
 
-  @Column('integer', { name: 'user_email_promotion', nullable: true })
+  @Column("integer", { name: "user_email_promotion", nullable: true })
   userEmailPromotion: number | null;
 
-  @Column('character varying', { name: 'user_demographic', nullable: true })
+  @Column("character varying", { name: "user_demographic", nullable: true })
   userDemographic: string | null;
 
-  @Column('timestamp without time zone', {
-    name: 'user_modified_date',
+  @Column("timestamp without time zone", {
+    name: "user_modified_date",
     nullable: true,
   })
   userModifiedDate: Date | null;
 
-  @Column('character varying', {
-    name: 'user_photo',
+  @Column("character varying", {
+    name: "user_photo",
     nullable: true,
     length: 255,
   })
   userPhoto: string | null;
 
-  @Column('integer', { name: 'user_current_role', nullable: true })
+  @Column("integer", { name: "user_current_role", nullable: true })
   userCurrentRole: number | null;
 
+  @OneToMany(
+    () => BatchTrainee,
+    (batchTrainee) => batchTrainee.batrTraineeEntity
+  )
+  batchTrainees: BatchTrainee[];
+
+  @OneToMany(
+    () => BatchTraineeEvaluation,
+    (batchTraineeEvaluation) => batchTraineeEvaluation.btevTraineeEntity
+  )
+  batchTraineeEvaluations: BatchTraineeEvaluation[];
+
+  @OneToMany(() => CartItems, (cartItems) => cartItems.caitUserEntity)
+  cartItems: CartItems[];
+
+  @OneToOne(() => Employee, (employee) => employee.empEntity)
+  employee: Employee;
+
+  @OneToMany(() => ProgramApply, (programApply) => programApply.prapUserEntity)
+  programApplies: ProgramApply[];
+
+  @OneToMany(
+    () => ProgramApplyProgress,
+    (programApplyProgress) => programApplyProgress.parogUserEntity
+  )
+  programApplyProgresses: ProgramApplyProgress[];
+
+  @OneToMany(
+    () => ProgramReviews,
+    (programReviews) => programReviews.prowUserEntity
+  )
+  programReviews: ProgramReviews[];
+
+  @OneToMany(
+    () => SalesOrderHeader,
+    (salesOrderHeader) => salesOrderHeader.soheUserEntity
+  )
+  salesOrderHeaders: SalesOrderHeader[];
+
+  @OneToMany(
+    () => TransactionPayment,
+    (transactionPayment) => transactionPayment.users
+  )
+  transactionPayments: TransactionPayment[];
+
   @OneToOne(() => BusinessEntity, (businessEntity) => businessEntity.users)
-  @JoinColumn([{ name: 'user_entity_id', referencedColumnName: 'entityId' }])
+  @JoinColumn([{ name: "user_entity_id", referencedColumnName: "entityId" }])
   userEntity: BusinessEntity;
+
+  @OneToMany(() => UsersAccount, (usersAccount) => usersAccount.usacUserEntityId)
+  usersAccounts: UsersAccount[];
 
   @OneToMany(() => UsersAddress, (usersAddress) => usersAddress.etadEntity)
   usersAddresses: UsersAddress[];
 
   @OneToMany(
     () => UsersEducation,
-    (usersEducation) => usersEducation.usduEntity,
+    (usersEducation) => usersEducation.usduEntity
   )
   usersEducations: UsersEducation[];
 
@@ -99,7 +157,7 @@ export class Users {
 
   @OneToMany(
     () => UsersExperiences,
-    (usersExperiences) => usersExperiences.usexEntity,
+    (usersExperiences) => usersExperiences.usexEntity
   )
   usersExperiences: UsersExperiences[];
 
